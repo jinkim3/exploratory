b571966b636984fe2913126178c8b5616842d400
unknown <jin.m.kim@yale.edu>
2023-10-10 05:00:45 -0400

    22

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,20 +1,21 @@
 # display the current version
 .onAttach <- function(libname, pkgname) {
-  # deal with 5 possible cases
-  # 1. error in getting the current package version -> update
-  # 2. error in getting the github package version -> update
-  # 3. current package version < github package version -> update
-  # 4. current package version > github package version -> do not update
-  # 5. current package version == github package version -> do not update
-  # in short, update unless the version numbers match
-
+  # notify the option to update? (5 possible cases)
+  # 1. error in getting the current package version -> yes
+  # 2. error in getting the github package version -> no
+  #    -> this used to be "yes," but as of aug 22, 2023, it's now "no"
+  # 3. current package version < github package version -> yes
+  # 4. current package version > github package version -> no
+  # 5. current package version == github package version -> no
+  # in short, notify the option to update unless the version numbers match
   # get version of the currently installed package
   current_pkg_version <- tryCatch(
     as.character(utils::packageVersion("exploratory")),
     error = function(e) "unknown")
   # github url
-  github_url <- paste0("https://raw.githubusercontent.com/jinkim3/",
-                       "exploratory/master/DESCRIPTION")
+  github_url <- paste0(
+    "https://raw.githubusercontent.com/jinkim3/",
+    "exploratory/master/DESCRIPTION")
   # get github description or handle errors
   github_pkg_desc <- tryCatch(
     readLines(github_url),
@@ -22,46 +23,57 @@
     error = function(e) {"github_desc_read_fail"})
   # get the version number of github version
   if (identical(github_pkg_desc, "github_desc_read_fail")) {
+    # case 2. error in getting the github package version -> no
     github_pkg_version <- "unknown"
+    startup_message <- paste0(
+      "Package attached: exploratory v", current_pkg_version,
+      "\nThe function failed to learn about the most recent version",
+      " available on\nGitHub (possbly due to a lack of ",
+      "Internet connection).",
+      "\n\nYou have an option to update the package ",
+      "with the\nfunction `update_exploratory()`. ",
+      "If you do so, it is recommended that you restart R.\n")
   } else {
+    # cases 1, 3-5.
     github_pkg_version <- gsub(
       ".*ersion: ", "", github_pkg_desc[
-        grep("ersion", github_pkg_desc)])
-  }
-  # compare versions
-  compare_version_result <- tryCatch(
-    utils::compareVersion(
-      current_pkg_version, github_pkg_version),
-    warning = function(w) {999}, # 999 indicates no need for update
-    error = function(e) {999})
-  # skip update for case 5
-  if (current_pkg_version != "unknown" &
+        grep("ersion", github_pkg_desc)][1])
+    # compare versions
+    compare_version_result <- tryCatch(
+      utils::compareVersion(
+        current_pkg_version, github_pkg_version),
+      warning = function(w) {999}, # 999 indicates no need for update
+      error = function(e) {999})
+    # case 5. current package version == github package version -> no
+    # skip update for case 5
+    if (current_pkg_version != "unknown" &
+        github_pkg_version != "unknown" &
+        compare_version_result == 0) {
+      startup_message <- paste0(
+        "Package attached: exploratory v", current_pkg_version,
+        " (same as the most recent version available through GitHub).")
+    } else if (
+      # case 4. current package version > github package version -> no
+      # skip update for case 4
+      current_pkg_version != "unknown" &
       github_pkg_version != "unknown" &
-      compare_version_result == 0) {
-    startup_message <- paste0(
-      "Package attached: exploratory v", current_pkg_version,
-      " (same as the most recent version available through GitHub).",
-      "\n\nIf you run into errors, please try restarting R.\n")
-  } else if (
-    # skip update for case 4
-    current_pkg_version != "unknown" &
-    github_pkg_version != "unknown" &
-    compare_version_result > 0) {
-    startup_message <- paste0(
-      "Package attached: exploratory v", current_pkg_version,
-      " (probably same as the most recent version ",
-      "available through GitHub).",
-      "\n\nIf you run into errors, please try restarting R.\n")
-  } else {
-    # simply notify of the OPTION to update the package
-    # this is simply a notification of the option to update,
-    # rather than a recommendation to update
-    startup_message <- paste0(
-      "Package attached: exploratory v", current_pkg_version,
-      ". Most recent version available on GitHub: v", github_pkg_version,
-      "\nYou have an OPTION to update the package ",
-      "by typing 'update_exploratory()'.",
-      " If you do so, make sure to restart R.")
+      compare_version_result > 0) {
+      startup_message <- paste0(
+        "Package attached: exploratory v", current_pkg_version,
+        " (probably the most recent version available through GitHub).")
+    } else {
+      # cases. 1 and 3.
+      # simply notify of the OPTION to update the package
+      # this is simply a notification of the option to update,
+      # rather than a recommendation to update
+      startup_message <- paste0(
+        "Package attached: exploratory v", current_pkg_version,
+        "; Most recent version available on GitHub: v",
+        github_pkg_version,
+        "\n\nYou have an option to update the package ",
+        "with the\nfunction `update_exploratory()`. ",
+        "If you do so, it is recommended that you restart R.\n")
+    }
   }
   packageStartupMessage(startup_message)
 }
--- R/update_exploratory.R
+++ R/update_exploratory.R
@@ -2,6 +2,9 @@
 #'
 #' Updates the current package 'exploratory' by installing the
 #' most recent version of the package from GitHub
+#' This function requires installing Package 'remotes' v2.4.2
+#' (or possibly a higher version) by Csardi et al. (2021),
+#' <https://cran.r-project.org/package=remotes>
 #'
 #' @param force logical. If \code{force = TRUE}, force installing the
 #' update. If \code{force = FALSE}, do not force installing the update.
@@ -28,19 +31,21 @@
 #'
 #' @export
 update_exploratory <- function(
-  force = TRUE,
-  upgrade_other_pkg = FALSE,
-  confirm = TRUE) {
+    force = TRUE,
+    upgrade_other_pkg = FALSE,
+    confirm = TRUE) {
   # 6 possible cases
   # 1. error in getting the current package version -> yes
-  # 2. error in getting the github package version -> yes
+  # 2. error in getting the github package version -> no
+  #    -> this used to be "yes," but as of aug 22, 2023, it's now "no"
   # 3. current package version < github package version -> yes
   # 4. current package version > github package version -> no
   # 5. current package version == github package version -> no
   # 6. the user forces the update -> yes
   # in short, notify the option to update unless the version numbers match
-
-  # update if user wants the update
+  #
+  # case 6. the user forces the update -> yes
+  # i.e., update if the user wants an update
   if (force == TRUE) {
     # unload the package exploratory
     while ("package:exploratory" %in% search()) {
@@ -49,7 +54,7 @@ update_exploratory <- function(
     remotes::install_github(
       "jinkim3/exploratory", force = force, upgrade = upgrade_other_pkg)
     # attach the package
-    exploratory::prep("exploratory", silent_if_successful = TRUE)
+    kim::prep("exploratory", silent_if_successful = TRUE)
   } else {
     # get version of the currently installed package
     current_pkg_version <- tryCatch(
@@ -64,56 +69,65 @@ update_exploratory <- function(
       readLines(github_url),
       warning = function(w) {"github_desc_read_fail"},
       error = function(e) {"github_desc_read_fail"})
+    if (!identical(github_pkg_desc, "github_desc_read_fail")) {
+      github_pkg_version <- "unknown"
+    }
     # get the version number of github version
     if (identical(github_pkg_desc, "github_desc_read_fail")) {
+      # case 2. error in getting the github package version -> no
       github_pkg_version <- "unknown"
     } else {
       github_pkg_version <- gsub(
         ".*ersion: ", "", github_pkg_desc[
           grep("ersion", github_pkg_desc)])
-    }
-    # compare versions
-    compare_version_result <- tryCatch(
-      utils::compareVersion(
-        current_pkg_version, github_pkg_version),
-      warning = function(w) {999}, # 999 indicates no need for update
-      error = function(e) {999})
-    # skip update for case 5
-    if (current_pkg_version != "unknown" &
+      # cases 1, 3-5.
+      # compare versions
+      compare_version_result <- tryCatch(
+        utils::compareVersion(
+          current_pkg_version, github_pkg_version),
+        warning = function(w) {999}, # 999 indicates no need for update
+        error = function(e) {999})
+      # 5. current package version == github package version -> no
+      # skip update for case 5
+      if (current_pkg_version != "unknown" &
+          github_pkg_version != "unknown" &
+          compare_version_result == 0) {
+        message(paste0(
+          "Current version of 'exploratory': v", current_pkg_version,
+          " (same as the most recent version available through GitHub)."))
+      } else if (
+        # 4. current package version > github package version -> no
+        # skip update for case 4
+        current_pkg_version != "unknown" &
         github_pkg_version != "unknown" &
-        compare_version_result == 0) {
-      message(paste0(
-        "Current version of 'exploratory': v", current_pkg_version,
-        " (same as the most recent version available through GitHub)."))
-    } else if (
-      # skip update for case 4
-      current_pkg_version != "unknown" &
-      github_pkg_version != "unknown" &
-      compare_version_result > 0) {
-      message(paste0(
-        "Current version of 'exploratory': v", current_pkg_version,
-        " (probably the most recent version available through GitHub)."))
-    } else {
-      # confirm update
-      if (confirm == TRUE) {
-        # ask the user to confirm
-        user_reply <- utils::menu(
-          c("Yes.", "No."),
-          title = "\nDo you want to try to update the package 'exploratory'?")
+        compare_version_result > 0) {
+        message(paste0(
+          "Current version of 'exploratory': v", current_pkg_version,
+          " (probably the most recent version available through GitHub)."))
       } else {
-        # if not asked, assume the user wants to update
-        user_reply <- 1
-      }
-      # update if user wants the update
-      if (user_reply == 1) {
-        # unload the package exploratory
-        while ("package:exploratory" %in% search()) {
-          unloadNamespace("exploratory")
+        # cases 1 and 3.
+        # confirm update
+        if (confirm == TRUE) {
+          # ask the user to confirm
+          user_reply <- utils::menu(
+            c("Yes.", "No."),
+            title =
+              "\nDo you want to try to update the package 'exploratory'?")
+        } else {
+          # if not asked, assume the user wants to update
+          user_reply <- 1
         }
-        remotes::install_github(
-          "jinkim3/exploratory", force = force, upgrade = upgrade_other_pkg)
-        # attach the package
-        exploratory::prep("exploratory", silent_if_successful = TRUE)
+        # update if user wants the update
+        if (user_reply == 1) {
+          # unload the package exploratory
+          while ("package:exploratory" %in% search()) {
+            unloadNamespace("exploratory")
+          }
+          remotes::install_github(
+            "jinkim3/exploratory", force = force, upgrade = upgrade_other_pkg)
+          # attach the package
+          kim::prep("exploratory", silent_if_successful = TRUE)
+        }
       }
     }
   }
